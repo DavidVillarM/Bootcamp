c0320ab487b58d098341f2c81c69ffc752dbb7cd
DavidVillarM <144356845+DavidVillarM@users.noreply.github.com>
2024-04-13 14:48:53 -0400

    [TAREA] sensei

--- MERN/Javascript/Ninja/script.js
+++ MERN/Javascript/Ninja/script.js
@@ -23,3 +23,21 @@ ninja.drinkSake();
 ninja.showStats();
 
 
+class Sensei extends Ninja{
+    constructor(nombre){
+        super(nombre, 200, 10, 10);
+        this.sabiduria = 10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+}
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+console.log(superSensei.nombre);
+superSensei.speakWisdom();
+// -> "Lo que un  programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
